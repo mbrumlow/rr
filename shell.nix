a591d9bf6b5cfeec45a4ee0a4bb6e5b6236fbb75
Mike Brumlow <mbrumlow@roblox.com>
2024-05-20 19:35:04 -0700

    Pinning packages

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/eb090f7b923b1226e8beb954ce7c8da99030f4a8.tar.gz";
+  }) {};
+  
+  
+in 
 with pkgs;
 mkShell rec {
     name = "go";
-    
     tools = [
       go
       buf
@@ -12,7 +19,6 @@ mkShell rec {
     libs = [
       stdenv.cc.cc
     ];
-
     buildInputs = tools ++ libs;
     LD_LIBRARY_PATH = lib.makeLibraryPath libs;
     GOROOT = "${pkgs.go}/share/go";
